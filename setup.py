cc99aa1b8243f901c3cae7716578955ea925a230
josh <n3rdcav3@gmail.com>
2015-09-19 22:08:47 -0400

    Created setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#
+# setup.py
+# Josh Artuso
+# Setup file to install the jbot module
+#
+
+from distutils.core import setup
+
+setup(name='jBot',
+      version='1.0',
+      description='Educational robot simulator',
+      author='Josh Artus',
+      url='https://github.com/frozenjava/RobotSimulator',
+      packages=['jbot'],
+      package_dir={'jbot': 'jbot'},
+      package_data={'jbot': ['images/*.png']}
+      )
